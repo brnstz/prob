3891e7281b18b4ca5059a3a2535919a05bdb3026
Brian Seitz <brnstz@gmail.com>
2016-02-27 16:17:00 -0500

    fix fatalf, add noreplaceordered

--- prob.go
+++ prob.go
@@ -2,13 +2,50 @@ package prob
 
 import "math/big"
 
-// ReplaceOrdered computes the number of possible uniquely ordered outcomes
+// ReplaceOrdered computes the possible number of uniquely ordered outcomes
 // when k choices are made from n possibilities, allowing for duplicate
-// values from n.
+// values from n. Mathmetically, this is: n^k
 func ReplaceOrdered(n, k *big.Int) *big.Int {
 	result := big.NewInt(0)
 
 	result.Exp(n, k, nil)
 
 	return result
+}
+
+// NoReplaceOrdered computes the possible number of ordered outcomes when k
+// choices are made from n possibilities, where values in n may only be chosen
+// once. Mathematically this is: n * (n - 1) * ... (n - k + 1)
+func NoReplaceOrdered(n, k *big.Int) *big.Int {
+	var result big.Int
+	var multiplier big.Int
+	var lastvalue big.Int
+
+	one := big.NewInt(1)
+
+	// Initialize result to n. This is effectively the first iteration.
+	result.Set(n)
+
+	// Initialize multiplier to n - 1. This will be the second iteration,
+	// assuming that n is large enough.
+	multiplier.Sub(n, one)
+
+	// Initialize lastvalue to n - k + 1
+	lastvalue.Sub(n, k)
+	lastvalue.Add(&lastvalue, one)
+
+	// Multiple values until multiplier is less than (n - k + 1)
+	for {
+		if multiplier.Cmp(&lastvalue) == -1 {
+			break
+		}
+
+		// Multiply the result by the multiplier
+		result.Mul(&result, &multiplier)
+
+		// Decrement the multiplier by 1
+		multiplier.Sub(&multiplier, one)
+	}
+
+	return &result
 }
--- prob_test.go
+++ prob_test.go
@@ -18,6 +18,7 @@ func TestReplaceOrdered(t *testing.T) {
 	}{
 		{5, 4, "625"},
 		{10, 2, "100"},
+		{0, 21, "0"},
 	}
 
 	// Go through each test
@@ -30,9 +31,40 @@ func TestReplaceOrdered(t *testing.T) {
 
 		// Check result matches or signal fatal error
 		if actual != test.result {
-			t.Fatal("expected prob.ReplaceOrdered(%d, %d) == %v but got %v",
+			t.Fatalf("expected prob.ReplaceOrdered(%d, %d) == %v but got %v",
 				test.n, test.k, test.result, actual,
 			)
 		}
 	}
+}
+
+func TestNoReplaceOrdered(t *testing.T) {
+	// tests is a list to run with the expectation that
+	// prob.NoReplaceOrdered(n, k) == result (where n and k are converted to
+	// big.Int values and the result is tested as a base10 string)
+	tests := []struct {
+		n      int64
+		k      int64
+		result string
+	}{
+		{100, 3, "970200"},
+		{5, 6, "0"},
+	}
+
+	// Go through each test
+	for _, test := range tests {
+
+		// Get the result as a base10 string
+		actual := prob.NoReplaceOrdered(
+			big.NewInt(test.n), big.NewInt(test.k),
+		).Text(10)
+
+		// Check result matches or signal fatal error
+		if actual != test.result {
+			t.Fatalf("expected prob.NoReplaceOrdered(%d, %d) == %v but got %v",
+				test.n, test.k, test.result, actual,
+			)
+		}
+	}
+
 }
